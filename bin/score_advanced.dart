575ecbec41c3cd7f410f0f51a245cac94de417fe
Hyodong90 <ct99610138@gmail.com>
2025-10-05 15:36:49 +0900

    1번 수정

--- bin/score_advanced.dart
+++ bin/score_advanced.dart
@@ -1,1 +1,40 @@
-void main(List<String> arguments) {}
+//점수만 출력하는 Score 정의
+
+class Score {
+  int score;
+
+  Score(this.score);
+
+  void show() {
+    print("점수: $score");
+  }
+}
+
+//Score를 상속받은 StudentScore 정의
+class StudentScore extends Score {
+  String name;
+
+// super 부모클래스 가져오기
+  StudentScore(this.name, int score) : super(score);
+
+  //부보 show 재정의
+  @override
+  void show() {
+    print("이름: $name, 점수: $score");
+  }
+}
+
+void main() {
+  // 기본 Score 클래스 사용
+  var a1 = Score(90);
+  a1.show();
+
+
+  print("-----------");
+  // 상속받은 StudentScore 클래스 사용
+  var a2 = StudentScore("홍길동", 90);
+  var a3 = StudentScore("김철수", 80);
+
+  a2.show();
+  a3.show();
+}
